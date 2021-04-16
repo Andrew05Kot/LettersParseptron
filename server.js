c9c1c58bbc6823659d7d49fa80e79770ec1555b4
Andrey <kotygaandrey05@gmail.com>
2021-04-16 20:00:23 +0300

    changed dependencies

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/letter-perceptron'));
+app.use(express.static(__dirname + '/dist/LetterParseptron'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
-    '/dist/letter-perceptron/index.html'));});
+    '/dist/LetterParseptron/index.html'));});
 app.listen(process.env.PORT || 8080);
